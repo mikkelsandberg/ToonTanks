35bd7f683f07a174f0359aa58250c7e022ceb62f
Mikkel <mikkel.h.sandberg@gmail.com>
2024-04-09 10:40:40 -0700

    Adding GameOver function

--- Source/ToonTanks/ToonTanksGameMode.cpp
+++ Source/ToonTanks/ToonTanksGameMode.cpp
@@ -16,10 +16,17 @@ void AToonTanksGameMode::ActorDied(AActor *DeadActor)
     {
       ToonTanksPlayerController->SetPlayerEnabledState(false);
     }
+    GameOver(false);
   }
   else if (ATower *DestroyedTower = Cast<ATower>(DeadActor))
   {
     DestroyedTower->HandleDestruction();
+    TargetTowers--;
+
+    if (TargetTowers == 0)
+    {
+      GameOver(true);
+    }
   }
 }
 
@@ -32,6 +39,7 @@ void AToonTanksGameMode::BeginPlay()
 
 void AToonTanksGameMode::HandleGameStart()
 {
+  TargetTowers = GetTargetTowerCount();
   Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
   ToonTanksPlayerController = Cast<AToonTanksPlayerController>(UGameplayStatics::GetPlayerController(this, 0));
 
@@ -53,4 +61,11 @@ void AToonTanksGameMode::HandleGameStart()
         StartDelay,
         false);
   }
+}
+
+int32 AToonTanksGameMode::GetTargetTowerCount()
+{
+  TArray<AActor*> Towers;
+  UGameplayStatics::GetAllActorsOfClass(this, ATower::StaticClass(), Towers);
+  return Towers.Num();
 }
